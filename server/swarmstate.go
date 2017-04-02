89631b7d52ecb5dbd9b292c1393f970061fe4a3a
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-04-02 22:38:11 +0200

    feat: random position attractor

--- server/swarmstate.go
+++ server/swarmstate.go
@@ -3,6 +3,7 @@ package server
 import (
 	"log"
 	"strconv"
+	"math/rand"
 
 	"github.com/netgusto/bytearena/utils"
 	uuid "github.com/satori/go.uuid"
@@ -20,9 +21,10 @@ type SwarmState struct {
 /* ***************************************************************************/
 
 func NewSwarmState() *SwarmState {
+
 	return &SwarmState{
 		Agents:           make(map[uuid.UUID](*AgentState)),
-		pin:              utils.NewVector2(200, 300).Clone(),
+		Pin:              utils.NewVector2(rand.Float64() * 200, rand.Float64() * 300).Clone(),
 		pendingmutations: lfreequeue.NewQueue(),
 	}
 }
