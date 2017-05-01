6354369b859d459a5cb4d9df9db65c9213f9e073
Jérôme Schneider / Net Gusto <contact@netgusto.com>
2017-05-01 23:15:56 +0200

    ./build.sh => ./compose.sh

--- compose.sh
+++ compose.sh
@@ -3,4 +3,4 @@
 GIT_ADMIN_KEY_PRIVATE=$(cat "$PWD/.keys/git_admin_key" | base64) \
 GIT_ADMIN_KEY_PUBLIC=$(cat "$PWD/.keys/git_admin_key.pub" | base64) \
 \
-docker-compose -f docker-compose.dev.yml up $1
+docker-compose -f docker-compose.dev.yml "$@"
