264a6e1cf8bbd07f08021bf8fadb8850780383d5
Jérôme Schneider <contact@netgusto.com>
2017-05-05 17:22:32 +0200

    Replacing gandalf with dotgit

--- compose.sh
+++ compose.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # TODO(sven): check for keys
 
-GIT_ADMIN_KEY_PRIVATE=$(cat "$PWD/.keys/git_admin_key" | base64) \
-GIT_ADMIN_KEY_PUBLIC=$(cat "$PWD/.keys/git_admin_key.pub" | base64) \
-\
-docker-compose -f docker-compose.dev.yml "$@"
+GIT_ADMIN_KEY_PRIVATE=$(cat "$PWD/.secrets/git_admin_key" | base64) \
+GIT_ADMIN_KEY_PUBLIC=$(cat "$PWD/.secrets/git_admin_key.pub" | base64) \
+export $(cat .secrets/params | xargs) && docker-compose -f docker-compose.dev.yml "$@"
--- docker/dotgit/start.sh
+++ docker/dotgit/start.sh
@@ -1,5 +1,8 @@
-echo "starting ssh service"
+echo "# Starting ssh service"
 service ssh start
 
-echo "starting git-daemon"
-su git -c "/usr/bin/git daemon --verbose --base-path=/home/git/repositories --export-all"
+echo "# Starting git-daemon"
+su git -c "/usr/bin/git daemon --verbose --base-path=/home/git/repositories --export-all" &
+
+echo "# Starting dotgit-api"
+su git -c "/usr/bin/dotgit-api"
