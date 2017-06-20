d25a63f130373a517b315ef045fc2e79a2e601a8
Sven SAULEAU <xtuc@users.noreply.github.com>
2017-06-20 17:59:55 +0200

    Update orchestrator.go

--- arenaserver/container/orchestrator.go
+++ arenaserver/container/orchestrator.go
@@ -184,7 +184,7 @@ func (orch *ContainerOrchestrator) CreateAgentContainer(agentid uuid.UUID, host 
 		CapDrop:        []string{"ALL"},
 		Privileged:     false,
 		AutoRemove:     true,
-		ReadonlyRootfs: true,
+		ReadonlyRootfs: false,
 		NetworkMode:    "bridge",
 		// Resources: container.Resources{
 		// 	Memory: 1024 * 1024 * 32, // 32M
