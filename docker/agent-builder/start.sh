ad30bb275e5b2db7a6f146f502c04daeae48bfe8
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-08-02 17:27:06 +0200

    feat: sshkey

--- docker/agent-builder/start.sh
+++ docker/agent-builder/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -x
+
+echo -n "$PRIVATE_KEY" | base64 -d > /root/git_admin_key_private
+cat /root/git_admin_key_private
+chmod 600 /root/git_admin_key_private
 
 /usr/bin/agent-builder
