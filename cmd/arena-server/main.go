5acc67fcd0b243d7aea6d810c2c4711a3dbf9cd5
Sven SAULEAU <xtuc@users.noreply.github.com>
2017-08-02 22:32:47 +0200

    Update main.go

--- cmd/arena-server/main.go
+++ cmd/arena-server/main.go
@@ -36,7 +36,7 @@ func main() {
 	port := flag.Int("port", 8080, "Port serving the arena")
 	mqhost := flag.String("mqhost", "mq:5678", "Message queue host:port")
 	apiurl := flag.String("apiurl", "http://bytearena.com/privateapi/graphql", "GQL API URL")
-	timeout := flag.Int("timeout", 60, "Limit the time of the game")
+	timeout := flag.Int("timeout", 60, "Limit the time of the game (in minutes)")
 
 	flag.Parse()
 
