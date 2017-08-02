11e764f9f02ae0b6008d8bd74337035c776ebb2b
Jérôme Schneider <contact@netgusto.com>
2017-08-02 23:12:25 +0200

    Viz: keep 5 seconds of stream in buffer instead of 1

--- common/protocol/streamstate.go
+++ common/protocol/streamstate.go
@@ -13,15 +13,19 @@ import (
 
 func StreamState(srv *arenaserver.Server, brokerclient mq.ClientInterface) {
 
-	buk := leakybucket.NewBucket(srv.GetTicksPerSecond(), srv.GetTicksPerSecond(), func(batch leakybucket.Batch, bucket *leakybucket.Bucket) {
-		frames := batch.GetFrames()
-		jsonbatch := make([]json.RawMessage, len(frames))
-		for i, frame := range frames {
-			jsonbatch[i] = json.RawMessage(frame.GetPayload())
-		}
+	buk := leakybucket.NewBucket(
+		srv.GetTicksPerSecond(),
+		srv.GetTicksPerSecond()*5, // keep 5 seconds of stream in buffer
+		func(batch leakybucket.Batch, bucket *leakybucket.Bucket) {
+			frames := batch.GetFrames()
+			jsonbatch := make([]json.RawMessage, len(frames))
+			for i, frame := range frames {
+				jsonbatch[i] = json.RawMessage(frame.GetPayload())
+			}
 
-		brokerclient.Publish("viz", "message", jsonbatch)
-	})
+			brokerclient.Publish("viz", "message", jsonbatch)
+		},
+	)
 
 	stateobserver := srv.SubscribeStateObservation()
 	for {
