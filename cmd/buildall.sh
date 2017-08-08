3d52ceafe10e87300a18b219e4e2939899654d05
Jérôme Schneider / Net Gusto <contact@netgusto.com>
2017-08-08 17:20:46 +0200

    Dynamic buildall.sh

--- cmd/buildall.sh
+++ cmd/buildall.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-set -x
+#set -x
 
-BUILDFLAGS="-v -a"
+BUILDFLAGS="-v"
+COMMANDS=$(find * -maxdepth 0 -type d)
 
-cd agent-builder && go build $BUILDFLAGS && cd .. && \
-cd arena-master && go build $BUILDFLAGS && cd .. && \
-cd arena-server && go build $BUILDFLAGS && cd .. && \
-cd arena-trainer && go build $BUILDFLAGS && cd .. && \
-cd dotgit-keystore && go build $BUILDFLAGS && cd .. && \
-cd dotgit-mq-consumer && go build $BUILDFLAGS && cd .. && \
-cd dotgit-ssh && go build $BUILDFLAGS && cd .. && \
-cd map-builder && go build $BUILDFLAGS && cd .. && \
-cd mq-cli && go build $BUILDFLAGS && cd .. && \
-cd viz-server && go build $BUILDFLAGS && cd .. && \
-echo "ALL BUILT !"
+for i in $COMMANDS
+do
+   : 
+   # do whatever on $i
+   echo "############################################################"
+   echo "# Building ${i}"
+   echo "############################################################"
+   echo ""
+   cd "$i" && go build $BUILDFLAGS && cd ..
+   echo ""
+done
+
