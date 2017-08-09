ca33b5fa0a326a6c77a53f0be50bd2d30412b5bb
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-08-09 13:13:15 +0200

    fix: undefined var

--- vizserver/handler/replay-websocket.go
+++ vizserver/handler/replay-websocket.go
@@ -22,7 +22,7 @@ func ReplayWebsocket(recorder recording.Recorder, basepath string) func(w http.R
 		vars := mux.Vars(r)
 		UUID := vars["recordId"]
 
-		recordFile := recorder.GetDirectory() + "/" + id
+		recordFile := recorder.GetDirectory() + "/" + UUID
 
 		_, err := os.Stat(recordFile)
 
