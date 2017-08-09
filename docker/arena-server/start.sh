4a6c8c6c4db5966f6c8dc0b667c27d5d3028e20a
Sven SAULEAU <xtuc@users.noreply.github.com>
2017-08-09 11:38:36 +0200

    Update start.sh

--- docker/arena-server/start.sh
+++ docker/arena-server/start.sh
@@ -11,3 +11,4 @@ function teardown {
 trap teardown EXIT
 
 exec /usr/bin/arena-server --port "${PORT}" --mqhost "${MQHOST}" --apiurl "${APIURL}" --id "$ID" --timeout "${GAME_TIMEOUT}" --registryAddr "${REGISTRY_ADDR}" --arenaAddr "${ARENA_ADDR}"
+teardown
