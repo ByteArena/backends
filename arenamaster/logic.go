22c994a5d42df0882b67d3eb1d3b3e8a21d61f5d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-08-23 22:51:28 +0200

    reactor: split onGameHandshake

--- arenamaster/logic.go
+++ arenamaster/logic.go
@@ -2,23 +2,8 @@ package arenamaster
 
 import (
 	"strconv"
-
-	"github.com/bytearena/bytearena/common/types"
-	"github.com/bytearena/bytearena/common/utils"
 )
 
 func getMasterStatus(state *State) string {
 	return "(" + strconv.Itoa(len(state.idleArenas)) + " arena(s) idle, " + strconv.Itoa(len(state.runningArenas)) + " arena(s) running, " + strconv.Itoa(len(state.pendingArenas)) + " arena(s) pending)"
-}
-
-func onGameHandshake(state *State, payload *types.MQPayload) {
-	id, ok := (*payload)["id"].(string)
-
-	if ok {
-		state.idleArenas[id] = ArenaState{
-			id: id,
-		}
-
-		utils.Debug("master", id+" joined "+getMasterStatus(state))
-	}
 }
