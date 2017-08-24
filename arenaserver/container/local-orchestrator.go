73a29161a551c4c98853690a4415a55c5e59bd46
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-08-24 16:40:14 +0200

    refactor(orch): rename t into commonTypes

--- arenaserver/container/local-orchestrator.go
+++ arenaserver/container/local-orchestrator.go
@@ -6,7 +6,7 @@ import (
 	"errors"
 	"log"
 
-	t "github.com/bytearena/bytearena/common/types"
+	commonTypes "github.com/bytearena/bytearena/common/types"
 	"github.com/bytearena/bytearena/common/utils"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
@@ -23,7 +23,7 @@ func getHostLocalOrch(orch *ContainerOrchestrator) (string, error) {
 	return res.IPAM.Config[0].Gateway, nil
 }
 
-func startContainerLocalOrch(orch *ContainerOrchestrator, ctner *AgentContainer, addTearDownCall func(t.TearDownCallback)) error {
+func startContainerLocalOrch(orch *ContainerOrchestrator, ctner *AgentContainer, addTearDownCall func(commonTypes.TearDownCallback)) error {
 
 	err := orch.cli.ContainerStart(
 		orch.ctx,
