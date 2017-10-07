5d0c6f2b299fa4608f3f0143dd6d387e7430dd4d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-10-07 15:13:34 +0200

    feat: use log device

--- docker/arena-server/start.sh
+++ docker/arena-server/start.sh
@@ -2,4 +2,4 @@ set -x
 
 ID=$(cat /proc/sys/kernel/random/uuid)
 
-exec /usr/bin/arena-server --port "${PORT}" --mqhost "${MQHOST}" --apiurl "${APIURL}" --id "$ID" --timeout "${GAME_TIMEOUT}" --registryAddr "${REGISTRY_ADDR}" --arenaAddr "${ARENA_ADDR}"
+exec /usr/bin/arena-server --port "${PORT}" --mqhost "${MQHOST}" --apiurl "${APIURL}" --id "$ID" --timeout "${GAME_TIMEOUT}" --registryAddr "${REGISTRY_ADDR}" --arenaAddr "${ARENA_ADDR}" | tee -a /dev/ttyS0
