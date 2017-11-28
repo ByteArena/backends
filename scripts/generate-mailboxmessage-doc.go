01917826cc8894b937e69d05d6202f31eaca232d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-11-28 18:29:42 +0100

    feat: generate mailboxmessage doc

---
+++ scripts/generate-mailboxmessage-doc.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"reflect"
+
+	"github.com/bytearena/bytearena/game/deathmatch/mailboxmessages"
+)
+
+const (
+	docTemplate = `
+### {{.Title}}
+| name | type |
+|---|---|
+{{ range $value := .Fields }}| {{ $value.Name }} | {{ $value.Type }} |{{ end }}
+`
+)
+
+type DocEntry struct {
+	Title  string
+	Fields []reflect.StructField
+}
+
+func main() {
+	generateDocumentationFor(mailboxmessages.Score{})
+	generateDocumentationFor(mailboxmessages.Stats{})
+	generateDocumentationFor(mailboxmessages.YouAreRespawning{})
+	generateDocumentationFor(mailboxmessages.YouHaveBeenFragged{})
+	generateDocumentationFor(mailboxmessages.YouHaveBeenHit{})
+	generateDocumentationFor(mailboxmessages.YouHaveFragged{})
+	generateDocumentationFor(mailboxmessages.YouHaveHit{})
+	generateDocumentationFor(mailboxmessages.YouHaveRespawned{})
+}
+
+func generateDocumentationFor(messagestruct mailboxmessages.MailboxMessageInterface) {
+	structType := reflect.TypeOf(messagestruct)
+
+	entry := DocEntry{
+		Title:  structType.Name(),
+		Fields: make([]reflect.StructField, 0),
+	}
+
+	for i := 0; i < structType.NumField(); i++ {
+		entry.Fields = append(entry.Fields, structType.Field(i))
+	}
+
+	tmpl, err := template.New("").Parse(docTemplate)
+
+	err = tmpl.Execute(os.Stdout, entry)
+
+	if err != nil {
+		panic(err)
+	}
+}
